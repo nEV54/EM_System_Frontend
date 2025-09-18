92d6f71c24faff29b5cb8d9f264708ac21374d16
Tushar Khandelwal <tushar.khandelwal@incture.com>
2025-09-18 18:25:19 +0530

    Commit changed

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useAuth } from '../context/authContext'
-import AdminSideBar from '../components/dashboard/AdminSideBar'
+import AdminSideBar from '../components/dashboard/AdminSidebar'
 import Navbar from '../components/dashboard/Navbar'
 import { Outlet } from 'react-router-dom'
+import AdminSidebar from '../components/dashboard/AdminSidebar'
 
 
 const AdminDashboard = () => {
@@ -11,7 +12,7 @@ const AdminDashboard = () => {
 
   return (
     <div className='flex bg-gray-100 min-h-screen'>
-      <AdminSideBar />
+      <AdminSidebar />
       <div className='flex-1 ml-64 flex flex-col'>
         <Navbar />
         <div className="p-6">
